988c9a6038408fe0fe999e3c3d3df32bba56f456
Idries Nasim <m.i.nasim@student.vu.nl>
2022-02-03 17:04:25 +0100

    fixed KG_analysis

--- hdt_analysis/KG_analysis.py
+++ hdt_analysis/KG_analysis.py
@@ -1,4 +1,10 @@
 from hdt import HDTDocument
+"""
+install 
+https://github.com/RDFLib/rdflib-hdt
+https://github.com/RDFLib/rdflib-hdt
+
+"""
 
 sameas = 'http://www.w3.org/2002/07/owl#sameAs'
 differentFrom = 'https://www.w3.org/2002/07/owl#differentFrom'
@@ -10,14 +16,14 @@ def count_triples(path):
     triples, cardinality_sameas = hdt_lod.search_triples("", sameas, "")
     triples, cardinality_differentFrom = hdt_lod.search_triples("", differentFrom, "")
     triples, cardinality_exactMatch = hdt_lod.search_triples("", exactMatch, "")
-    #triples, cardinality_total = hdt_lod.search_triples("", "", "")
+    triples, cardinality_total = hdt_lod.search_triples("", "", "")
 
     print("count of owl:sameAs triples: \n", cardinality_sameas)
     print("count of owl:differentFrom triples: \n", cardinality_differentFrom)
     print("count of skos:exactMatch triples: \n", cardinality_exactMatch)
-    print("count of total triples: \n", hdt.total_triples)
+    print("count of total triples: \n", cardinality_total)
 
 count_triples("integrated.hdt")
 
-PATH_LOD = "/scratch/wbeek/data/LOD-a-lot/data.hdt"
-count_triples(PATH_LOD)
+# PATH_LOD = "/scratch/wbeek/data/LOD-a-lot/data.hdt"
+# count_triples(PATH_LOD)
