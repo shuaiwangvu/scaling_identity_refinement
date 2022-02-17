cd42c8ea2059b69359ce23913a686a87f541cefb
Idries Nasim <m.i.nasim@student.vu.nl>
2022-02-17 17:17:59 +0100

    updated scripts; moved report.md to shared doc drive

--- data/nr_identity_and_seeAlso.py
+++ data/nr_identity_and_seeAlso.py
@@ -8,15 +8,15 @@ def count_triples(path, format):
 
     count_sameAs = 0
     count_seeAlso = 0
-    
-    for _ in g.triples((None, URIRef(sameAs), None)):
+
+    for _,_,_ in g.triples((None, URIRef(sameAs), None)):
         count_sameAs += 1
-    for _ in g.triples((None, URIRef(seeAlso), None)):
+    for _,_,_ in g.triples((None, URIRef(seeAlso), None)):
         count_seeAlso += 1
 
     print(path, " has ", len(g), " triples and ", count_sameAs , " amount of sameAs triples and ", count_seeAlso, " rdf seeAlso triples.")
 
-files= ['mappingbased-objects-uncleaned.ttl']
+files= ['commons-sameas-links_lang=nl.ttl']
 
 for file in files:
     count_triples(file, file.split(".")[-1])
--- data/nr_triples.py
+++ data/nr_triples.py
@@ -19,17 +19,24 @@ def count_triples(path, format):
             new_g.add((s,p,o))
     print(path, " has ", len(g), " triples and ", count , " amount of sameAs triples")
 
-files= ['yago-wd-sameAs.nt','sameas-external.ttl','commons-sameas-links_lang=es.ttl',
-'commons-sameas-links_lang=ja.ttl','commons-sameas-links_lang=pt.ttl',
-'commons-sameas-links_lang=nl.ttl','commons-sameas-links_lang=de.ttl',
-'commons-sameas-links_lang=en.ttl','commons-sameas-links_lang=fr.ttl',
-'sameas-all-wikis.ttl','commons-sameas-links_lang=ja-1.ttl',
-'commons-sameas-links_lang=es-1.ttl','commons-sameas-links_lang=de-1.ttl',
-'commons-sameas-links_lang=en-1.ttl','commons-sameas-links_lang=fr-1.ttl',
-'commons-sameas-links_lang=pt-1.ttl','commons-sameas-links_lang=nl-1.ttl',
-'sameas-external-1.ttl']
+files= [
+'identity_links/commons-sameas-links_lang=es.ttl','identity_links/commons-sameas-links_lang=ja.ttl',
+'identity_links/commons-sameas-links_lang=pt.ttl','identity_links/commons-sameas-links_lang=nl.ttl',
+'identity_links/commons-sameas-links_lang=de.ttl','identity_links/commons-sameas-links_lang=en.ttl',
+'identity_links/commons-sameas-links_lang=fr.ttl','identity_links/commons-sameas-links_lang=ja-1.ttl',
+'identity_links/commons-sameas-links_lang=es-1.ttl','identity_links/commons-sameas-links_lang=de-1.ttl',
+'identity_links/commons-sameas-links_lang=en-1.ttl','identity_links/commons-sameas-links_lang=fr-1.ttl',
+'identity_links/commons-sameas-links_lang=pt-1.ttl','identity_links/commons-sameas-links_lang=nl-1.ttl',
+]
 
 for file in files:
     count_triples(file, file.split(".")[-1])
 
-new_g.serialize(destination="total_identity_links.ttl")
+new_g.serialize(destination="identity_graph.nt")
+
+g = Graph().parse('identity_graph.nt', format='nt')
+
+count_sameAs = 0
+for _,_,_ in g.triples((None, URIRef(sameAs), None)):
+    count_sameAs += 1
+print('identity_graph.nt has ', len(g), " triples and ", count_sameAs , " amount of sameAs triples")
