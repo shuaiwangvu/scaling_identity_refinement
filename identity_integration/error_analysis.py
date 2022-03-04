381e856295e7be7b2c0ee048814e052567d95d0c
shuaiwangvu <shuai.wang.vu@gmail.com>
2022-03-04 13:28:54 +0100

    perform analysis of the erroneous triples

---
+++ identity_integration/error_analysis.py
@@ -0,0 +1,3 @@
+#files I skipped
+#[‘identity_links/yago-wd-sameAs.nt','identity_links/sameas-external.ttl'',
+# 'identity_links/sameas-all-wikis.ttl','identity_links/sameas-external-1.ttl', 'mappingbased-objects-uncleaned.ttl']
