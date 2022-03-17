e5d1034c7a8856ec93ca2e1410f6b889135633bd
Idries Nasim <m.i.nasim@student.vu.nl>
2022-03-18 00:43:11 +0100

    fixed bug in statistics_metalink.py, started working on extend_metalink.py

--- hdt_analysis/extend_metalink.py
+++ hdt_analysis/extend_metalink.py
@@ -5,6 +5,7 @@ rdf_subject = "http://www.w3.org/1999/02/22-rdf-syntax-ns#subject"
 rdf_object = "http://www.w3.org/1999/02/22-rdf-syntax-ns#object"
 
 count_none = 0
+max_id = 0
 PATH_META = "/home/jraad/ssd/data/identity/metalink/metalink.hdt"
 hdt_metalink = HDTDocument(PATH_META)
 
@@ -24,17 +25,27 @@ def find_statement_id(subject, object):
     inter_section = collect_statement_id_regarding_subject.intersection(collect_statement_id_regarding_object)
 
     if len(inter_section) == 1:
-        return inter_section
+        return list(inter_section)[0]
     else:
         return None
 
 triples, cardinality = hdt_metalink.search_triples("", "", "")
 
+#first loop check for the maxID, what if there is no triple with a id?
 for (s, _, o) in triples:
     id = find_statement_id(s,o)
-    if id == None:
-        count_none += 1
-    else:
-        pass
+    if (id > max_id):
+        max_id = int(id.split("/")[-1])
 
-print("nr of triples without an id", count_none)
+#second loop extends the metalink
+for (s, _, o) in triples:
+    max_id += 1
+    id = find_statement_id(s,o)
+    if id == None:
+        #add new triples here?
+        #https://docs.google.com/document/d/1ruA1xGLNn0SG8vORaZaeRcAMFLy55Pa9_EL9rAGi1Lg/edit#
+        md5 = 'https://krr.triply.cc/krr/metalink/fileMD5/' + str(max_id) #need to pass the max_id through a md5 generator first?
+        #new graph.add(md5, rdf_subject, s) 
+        #new graph.add(md5, rdf_object, o)
+        #nothing with the predicate? or do we assume everything is sameAs?
+        #eventually more meta Data
