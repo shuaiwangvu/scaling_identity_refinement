681440a76ad67f2437b08863653fc04f71958a09
Idries Nasim <m.i.nasim@student.vu.nl>
2022-05-29 02:32:57 +0200

    generalize rdflib script

--- identity_integration/convert_to_nt_using_rdflib.py
+++ identity_integration/convert_to_nt_using_rdflib.py
@@ -1,33 +1,35 @@
 """
 convert file from ttl to nt using rdflib (parser will load entire file into memory, not useful for large files)
 """
-from rdflib import Graph, URIRef
+from rdflib import Graph
+import sys
 
-sameas = 'http://www.w3.org/2002/07/owl#sameAs'
-exactmatch = 'http://www.w3.org/2004/02/skos/core#exactMatch'
+SAMEAS = 'http://www.w3.org/2002/07/owl#sameAs'
+SAMEAS_1 = 'http://schema.org/sameAs'
+EXACTMATCH = 'http://www.w3.org/2004/02/skos/core#exactMatch'
 
 
-def count_triples(path, new_g):
+def count_triples_and_serialize(path, new_g):
+	new_g = Graph()
 	g = Graph()
 
-	g.parse(path, format="turtle", encoding='utf-8')
+	g.parse(path)
 	
 
 	count_owl = 0
 	count_skos = 0
 
 	for s,p,o in g.triples((None, None, None)):
-		if p.toPython() == sameas:
+		if p.toPython() == SAMEAS or p.toPython() == SAMEAS_1:
 			count_owl += 1
 			new_g.add((s,p,o))
-		elif p.toPython() == exactmatch:
+		elif p.toPython() == EXACTMATCH:
 			count_skos += 1
 			new_g.add((s,p,o))
-	print(path, " has ", len(g), " triples and ", count_owl , " amount of sameAs triples,", count_skos , " amount of skos:exacthMatch triples")
 
-files = ["commons-sameas-links_lang=de.ttl","commons-sameas-links_lang=en.ttl","commons-sameas-links_lang=es.ttl","commons-sameas-links_lang=fr.ttl","commons-sameas-links_lang=ja.ttl",
-"commons-sameas-links_lang=nl.ttl","commons-sameas-links_lang=pt.ttl","sameas-all-wikis.ttl","sameas-external.ttl"]
-for file in files:
-	new_g = Graph()
-	count_triples(file, new_g)
+	print(path, " has ", len(g), " triples and ", count_owl , " amount of sameAs triples,", count_skos , " amount of skos:exacthMatch triples")
 	new_g.serialize(destination = f"converted_nt/{file.split('.')[0]}.nt", format="nt11", encoding='utf-8')
+
+files = sys.argv[1:]
+for file in files:
+	count_triples_and_serialize(file)
