8da9b309357c7585009a7b66864e9a3962880567
Idries Nasim <m.i.nasim@student.vu.nl>
2022-06-21 23:20:59 +0200

    forgot timer but lasted only a few minutes

--- count_cc_2_tsv.py
+++ count_cc_2_tsv.py
@@ -23,3 +23,9 @@ with open('size_cc_2_number_of_occurences.tsv', 'w') as out:
     tsv = csv.writer(out, delimiter="\t")
     for k,v in it:
         tsv.writerow([k.decode(),v.decode()])
+
+end = time.time()
+hours, rem = divmod(end-start, 3600)
+minutes, seconds = divmod(rem, 60)
+time_formated = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
+print("Time taken = ", time_formated)
