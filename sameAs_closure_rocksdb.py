3991447c02653c7f725812f974c7539443f2d0d6
Idries Nasim <m.i.nasim@student.vu.nl>
2022-06-21 03:02:16 +0200

    added merge class

--- sameAs_closure_rocksdb.py
+++ sameAs_closure_rocksdb.py
@@ -4,14 +4,23 @@ from collections import Counter
 import csv
 import time
 import rocksdb
-from rocksdb.merge_operators import StringAppendOperator
+
+class AppendEntity(rocksdb.interfaces.AssociativeMergeOperator):
+    def merge(self, key, existing_value, value):
+        if existing_value:
+            s = existing_value + b' ' + value
+            return (True, s)
+        return (True, value)
+
+    def name(self):
+        return b'AppendEntity'
 
 for file in sys.argv[1:]:
     print(f'processing {file}')
 
     opts = rocksdb.Options()
     opts.create_if_missing = True
-    opts.merge_operator = StringAppendOperator()
+    opts.merge_operator = AppendEntity()
     identity_set = rocksdb.DB(f"{file.split('.')[0]}_identity_set_rocksdb.db", opts)
     mapping_IS = rocksdb.DB(f"{file.split('.')[0]}_mapping_IS_rocksdb.db", rocksdb.Options(create_if_missing=True))
 
@@ -27,22 +36,24 @@ for file in sys.argv[1:]:
         x_id = mapping_IS.get(x)
         y_id = mapping_IS.get(y)
         if not x_id and not y_id:
-            mapping_IS.put(x, str(unique_key).encode())
-            mapping_IS.put(y, str(unique_key).encode())
-            identity_set.put(str(unique_key).encode(), x + b" " + y)
+            curr_key_id = str(unique_key).encode()
+            mapping_IS.put(x, curr_key_id)
+            mapping_IS.put(y, curr_key_id)
+            identity_set.put(curr_key_id, x + b" " + y)
             unique_key += 1
         elif x_id and not y_id:
             mapping_IS.put(y, x_id)
-            identity_set.merge(x_id, b" "+ y)
+            identity_set.merge(x_id, y)
         elif not x_id and y_id:
             mapping_IS.put(x, y_id)
-            identity_set.merge(y_id, b" " + x)
+            identity_set.merge(y_id, x)
         elif x_id and y_id and x_id != y_id:
             IS_of_y = identity_set.get(y_id)
-            identity_set.delete(y_id)
-            identity_set.merge(x_id, b" " + IS_of_y)
+            identity_set.merge(x_id, IS_of_y)
             for mapping_key in IS_of_y.split():
+                # mapping_IS.delete(mapping_key)
                 mapping_IS.put(mapping_key, x_id)
+            identity_set.delete(y_id)
     # print("identity set:")
     # it = identity_set.iteritems()
     # it.seek_to_first()
