72cb809a0a751de637c0017a9e4a84610e39f3a2
Idries Nasim <m.i.nasim@student.vu.nl>
2022-06-21 02:19:37 +0200

    updated rocksdb

--- sameAs_closure_rocksdb.py
+++ sameAs_closure_rocksdb.py
@@ -4,11 +4,17 @@ from collections import Counter
 import csv
 import time
 import rocksdb
+from rocksdb.merge_operators import StringAppendOperator
 
 for file in sys.argv[1:]:
     print(f'processing {file}')
-    mapping_IS = rocksdb.DB(f"{file.split('.')[0]}_mapping_IS.db", rocksdb.Options(create_if_missing=True))
-    identity_set = rocksdb.DB(f"{file.split('.')[0]}_identity_set.db", rocksdb.Options(create_if_missing=True))
+
+    opts = rocksdb.Options()
+    opts.create_if_missing = True
+    opts.merge_operator = StringAppendOperator()
+    identity_set = rocksdb.DB(f"{file.split('.')[0]}_identity_set_rocksdb.db", opts)
+    mapping_IS = rocksdb.DB(f"{file.split('.')[0]}_mapping_IS_rocksdb.db", rocksdb.Options(create_if_missing=True))
+
     hdt_metalink = HDTDocument(file)
     start = time.time()
     unique_key = 0
@@ -36,9 +42,15 @@ for file in sys.argv[1:]:
             identity_set.delete(y_id)
             identity_set.merge(x_id, b" " + IS_of_y)
             for mapping_key in IS_of_y.split():
-                mapping_IS.delete(mapping_key)
+                # mapping_IS.delete(mapping_key)
                 mapping_IS.put(mapping_key, x_id)
-      
+    print("identity set:")
+    for k,v in identity_set.iteritems():
+        print(f"{k} => {v}")
+    print("mapping IS:")
+    for k,v in mapping_IS.iteritems():
+        print(f"{k} => {v}")
+
     print(f'finished processing {file}')
     end = time.time()
     hours, rem = divmod(end-start, 3600)
