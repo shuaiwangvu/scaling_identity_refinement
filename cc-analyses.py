ad12e3a36c37281bbce5f0c66897897db25a6802
Idries Nasim <m.i.nasim@student.vu.nl>
2022-06-21 18:14:51 +0200

    fixed naming, added merge op?

--- cc-analyses.py
+++ cc-analyses.py
@@ -3,8 +3,19 @@ import time
 import rocksdb
 
 start = time.time()
+class AppendEntity(rocksdb.interfaces.AssociativeMergeOperator):
+    def merge(self, key, existing_value, value):
+        if existing_value:
+            s = existing_value + b' ' + value
+            return (True, s)
+        return (True, value)
 
-with rocksdb.DB("compacted_kg_identity_set.db", rocksdb.Options()) as identity_set:
+    def name(self):
+        return b'AppendEntity'
+
+opts = rocksdb.Options()
+opts.merge_operator = AppendEntity()
+with rocksdb.DB("compacted_kg_identity_set_rocksdb.db", opts) as identity_set:
     it = identity_set.iteritems()
     it.seek_to_first()
     with open('id2terms.tsv', 'w') as out:
@@ -12,7 +23,7 @@ with rocksdb.DB("compacted_kg_identity_set.db", rocksdb.Options()) as identity_s
         for k,v in it:
             tsv.writerow([k.decode(),v.decode()])
         
-with rocksdb.DB("compacted_kg_mapping_IS.db", rocksdb.Options()) as mapping_IS:
+with rocksdb.DB("compacted_kg_mapping_IS_rocksdb.db", rocksdb.Options()) as mapping_IS:
     it = mapping_IS.iteritems()
     it.seek_to_first()
     with open('terms2id.tsv', 'w') as out:
