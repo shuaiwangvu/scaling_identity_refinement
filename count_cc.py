0d75dcf9984d0f798445da2d8049e4b0ef167ec5
Idries Nasim <m.i.nasim@student.vu.nl>
2022-06-21 22:37:26 +0200

    added merge for IS

--- count_cc.py
+++ count_cc.py
@@ -3,6 +3,15 @@ import time
 import rocksdb
 
 start = time.time()
+class AppendEntity(rocksdb.interfaces.AssociativeMergeOperator):
+    def merge(self, key, existing_value, value):
+        if existing_value:
+            s = existing_value + b' ' + value
+            return (True, s)
+        return (True, value)
+
+    def name(self):
+        return b'AppendEntity'
 
 class AssocCounter(rocksdb.interfaces.AssociativeMergeOperator):
     def merge(self, key, existing_value, value):
@@ -13,17 +22,16 @@ class AssocCounter(rocksdb.interfaces.AssociativeMergeOperator):
 
     def name(self):
         return b'AssocCounter'
-        
 opts = rocksdb.Options()
 opts.create_if_missing = True
 opts.merge_operator = AssocCounter()
-identity_set = rocksdb.DB("compacted_kg_identity_set_rocksdb.db", rocksdb.Options())
+identity_set = rocksdb.DB("compacted_kg_identity_set_rocksdb.db", rocksdb.Options(merge_operator=AppendEntity()))
 counter =  rocksdb.DB("count_cc_size_and_occurence.db", opts)
 it = identity_set.iteritems()
 it.seek_to_first()
 for k,v in it:
     len_cc = len(v.decode().split())
-    counter.merge(len_cc, b"1")
+    counter.merge(str(len_cc).encode(), b"1")
 
 end = time.time()
 hours, rem = divmod(end-start, 3600)
