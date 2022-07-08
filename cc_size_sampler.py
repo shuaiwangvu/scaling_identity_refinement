9a56e8a4b9a03c70fcbf4a412607deb69b45d033
Idries Nasim <m.i.nasim@student.vu.nl>
2022-07-08 05:06:53 +0200

    sample based on cc size

--- cc_size_sampler.py
+++ cc_size_sampler.py
@@ -1,24 +1,52 @@
 # Util functions from Lars H. https://github.com/Lars-H/hdt_sampler
 
-# import rocksdb
+import rocksdb
 import sys
 from hdt import HDTDocument
 import random
 from rdf_utils import tuple_to_triple, tuple_to_ntriple
 import datetime as dt
 
+class AppendEntity(rocksdb.interfaces.AssociativeMergeOperator):
+    def merge(self, key, existing_value, value):
+        if existing_value:
+            s = existing_value + b' ' + value
+            return (True, s)
+        return (True, value)
+
+    def name(self):
+        return b'AppendEntity'
+
 def random_sample(file, size):
+    count_cc_two = 0
+    count_cc_Between_3_and_10 = 0
+    count_cc_larger_than_ten = 0
+    identity_set = rocksdb.DB("compacted_kg_identity_set_rocksdb.db", rocksdb.Options(merge_operator=AppendEntity()))
+
     (_, CARD) = file.search_triples("", "", "")
     cardinality = CARD
     sample = set()
     if size > CARD:
         raise Exception("Sample size exceeds dataset size")
-    while len(sample) < size:
+    while 1:
         offset = random.randint(0, cardinality - 1)
         (triples, res_card) = file.search_triples("", "", "", limit=1, offset=offset)
-        sample.add(tuple_to_triple(next(triples)))
-
-    return list(sample)
+        spo = next(triples)
+        s,_,_ = spo
+        len_cc = len(identity_set[s].decode().split())
+        if len_cc == 2 and count_cc_two < size:
+            count_cc_two += 2
+            sample.add(tuple_to_triple(spo))
+        elif len_cc > 2 and len_cc <= 10 and count_cc_Between_3_and_10 < size:
+            count_cc_Between_3_and_10 += 2
+            sample.add(tuple_to_triple(spo))
+        elif len_cc > 10 and count_cc_larger_than_ten < size:
+            count_cc_larger_than_ten += 2
+            sample.add(tuple_to_triple(spo))
+        
+        if count_cc_two == count_cc_Between_3_and_10 == count_cc_larger_than_ten == size:
+            return list(sample)
+    
 
 FILENAME = sys.argv[1]
 MAX_ENTITIES = int(sys.argv[2])
