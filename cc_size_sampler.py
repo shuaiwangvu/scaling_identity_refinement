36c6d8a4bcefaeb36845f4935dce3ad929d54e90
Idries Nasim <m.i.nasim@student.vu.nl>
2022-07-08 05:16:47 +0200

    added mapping to get idx for identity set

--- cc_size_sampler.py
+++ cc_size_sampler.py
@@ -21,8 +21,8 @@ def random_sample(file, size):
     count_cc_two = 0
     count_cc_Between_3_and_10 = 0
     count_cc_larger_than_ten = 0
-    identity_set = rocksdb.DB(f"{file.split('.')[0]}_identity_set_rocksdb.db", rocksdb.Options(merge_operator=AppendEntity()))
-
+    identity_set = rocksdb.DB("compacted_kg_identity_set_rocksdb.db", rocksdb.Options(merge_operator=AppendEntity()))
+    mapping_IS = rocksdb.DB("compacted_kg_mapping_IS_rocksdb.db", rocksdb.Options(create_if_missing=False))
     (_, CARD) = file.search_triples("", "", "")
     cardinality = CARD
     sample = set()
@@ -33,7 +33,8 @@ def random_sample(file, size):
         (triples, res_card) = file.search_triples("", "", "", limit=1, offset=offset)
         spo = next(triples)
         s,_,_ = spo
-        len_cc = len(identity_set.get(s.encode()).decode().split())
+        idx = mapping_IS.get(s.encode())
+        len_cc = len(identity_set.get(idx).decode().split())
         if len_cc == 2 and count_cc_two < size:
             count_cc_two += 2
             sample.add(tuple_to_triple(spo))
