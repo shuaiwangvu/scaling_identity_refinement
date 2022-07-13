abc5c4cdc5a425f68645ee36795c367c9a75925c
Idries Nasim <m.i.nasim@student.vu.nl>
2022-07-13 02:53:14 +0200

    debugged

--- sampling/rocksDB_keys2csv.py
+++ sampling/rocksDB_keys2csv.py
@@ -3,6 +3,7 @@
 import argparse
 import rocksdb
 import csv
+import time
 
 def get_options():
     parser = argparse.ArgumentParser(description="rocksDB_keys2csv: iterates through all keys in a database, and writes them to a csv file")
@@ -15,11 +16,20 @@ def get_options():
 
 options = get_options()
 
+start = time.time()
+
 db = rocksdb.DB(options.file, rocksdb.Options(create_if_missing=False))
 it = db.iterkeys()
 it.seek_to_first()
 
-with open(options.output_file, 'w') as output_file:
+with open(options.output_file, 'w', encoding='utf-8', newline='') as output_file:
     writer = csv.writer(output_file)
     for key in it:
-        writer.writerow(key)
+        writer.writerow(key.decode())
+
+print(f'finished processing')
+end = time.time()
+hours, rem = divmod(end-start, 3600)
+minutes, seconds = divmod(rem, 60)
+time_formated = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
+print("Time taken = ", time_formated)
